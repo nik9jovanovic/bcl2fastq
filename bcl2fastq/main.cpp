47274920b5957a51b2bf545a31a7aa4cdd9fb693
Nikola Jovanovic <nikola.jovanovic@sbgenomics.com>
2018-07-11 13:45:21 +0200

    Initial Commit

---
+++ bcl2fastq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bcl2fastq
+//
+//  Created by Nikola Jovanovic on 7/11/18.
+//  Copyright © 2018 Nikola Jovanovic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
